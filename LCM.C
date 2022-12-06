28af9258b624e0f7699ff8afca6f941a6b5b599e
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-12-06 23:25:08 +0530

    Update LCM.C

--- LCM.C
+++ LCM.C
@@ -4,9 +4,8 @@ void main()
 {
 int num1=20,num2=24,n,i,lcm,hcf;
 clrscr();
-	n=(num1<num2) ? num1: num2;
-
-	for(i=1;i<n;i++)
+	
+	for(i=1;i<num1 && i<=num2;i++)
 	{
 		if(num1%i==0 && num2%i==0)
 		{
